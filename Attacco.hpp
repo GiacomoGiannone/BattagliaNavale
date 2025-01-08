3b836299afb9217d6867471a9dc82714ea0c58de
Giacomo <giacomogiannone02@gmail.com>
2025-01-08 08:31:17 +0100

    -Aggiunto la classe  di intestazione "Attacco.hpp"

---
+++ Attacco.hpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+class Attacco
+{
+private:
+	int coordinata_x;
+	char coordinata_y;
+public:
+	Attacco(int _coordinata_x, char _coordinata_y);
+	int getCoordinataX();
+	char getCoordinataY();
+};
--- Griglia.hpp
+++ Griglia.hpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include "Giocatore.hpp"
 #include "Casella.hpp"
+#include "Attacco.hpp"
 
 class Griglia
 {
@@ -9,6 +10,7 @@ private:
 	std::pair<int, int> dim;
 	Giocatore g;
 	std::vector<std::vector<Casella>> ListaCaselle;
+	std::vector<Attacco> ListaAttacchi;
 public:
 	bool ScegliPosizione(int x, char y, std::string direction, int dim);
 	Casella FindCasella(int x, char y);
