5219ae8dfdecaacbc581424cf4ef7aec4bf1a226
Giacomo <giacomogiannone02@gmail.com>
2025-01-29 10:16:26 +0100

    Aggiunto nuovamente i colori ma stavolta compatibili per tutti

--- Casella.cpp
+++ Casella.cpp
@@ -5,19 +5,23 @@ std::ostream& operator<<(std::ostream& os, const Casella& casella)
     switch (casella.stato.getStato())
     {
     case StatoCasella::Stato::acqua:
+        os << "\033[34m"; // Blu
         os << casella.coordinata_x << casella.coordinata_y << "(A)";
         break;
     case StatoCasella::Stato::occupata:
+        os << "\033[33m"; // Giallo
         os << casella.coordinata_x << casella.coordinata_y << "(N)";
         break;
     case StatoCasella::Stato::colpita:
+        os << "\033[31m"; // Rosso
         os << "(X)";
         break;
     default:
+        os << "\033[0m";
         os << "Stato non valido";
         break;
     }
-    //SetColor(7);
+    os << "\033[0m"; //restta il colore da un'iterazione all'altra per stampare i messaggi normali
     return os;
 }
 
